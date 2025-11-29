19d5a7c034730832af6da64baabd2a315d92b246
act2504tamaki <newfarmb2024@gmail.com>
2025-11-30 07:55:41 +0900

    Add files via upload

---
+++ src/components/Form.tsx
@@ -0,0 +1,15 @@
+type FormProps = {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const Form = (props: FormProps) => {
+  return (
+    <form onSubmit={props.getWeather}>
+      <input type="text" name="city" placeholder="都市名を英語で入力" onChange={(e) => props.setCity(e.target.value)} />
+      <button type="submit">Get Weather</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h1>Weather - Umigame Counceling -</h1>;
+};
+
+export default Title;
